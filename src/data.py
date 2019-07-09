b6345312414e583730bbba1feee58f4eab5fece3
Lexxi-Smith <aas492@nau.edu>
2019-07-08 22:42:23 -0700

    new files

--- src/data.py
+++ src/data.py
@@ -10,6 +10,16 @@ def genRan(i):
         z.append(j)       
         y=y+1
     
+    length=len(z)
+
+    for i in range(length):
+        for j in range(0,length-i-1):
+            if z[j] > z[j+1]:
+                z[j], z[j+1] = z[j+1], z[j]
+
+
+    
+    
     return z
         
 genRan(x)

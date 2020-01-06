d8272e688fd9bbd8764e206bb520e2756fa5ceae
Lexxi-Smith <51714476+Lexxi-Smith@users.noreply.github.com>
2020-01-06 11:05:43 -0700

    Update balancedBST.py

--- src/balancedBST.py
+++ src/balancedBST.py
@@ -1,10 +1,31 @@
+# BST Implementation 
+# Need to implement the following:
+    # search
+    # remove 
+    # add
+    
+
+
 class Node:
-    def __init__(self,d):
-        self.data = d
-        self.left = None
-        self.right = None
+    rightChild,leftChild,data = None, None, None
+    def __init__(self,key):
+        self.leftChild = None
+        self.rightChild = None
+        self.data = key
 
-def SortArray(z):
-
+class Tree: 
+    root = None
+    size = 0
+    def __init__(self):
+        self.size = 0
+        self.root = None
+        
+    def insert(self,node,num):
+        return
+    def remove(self,node,num):
+        return 
+    def search(self,node,num):
+        return
+    def print_tree(self)
 
 
